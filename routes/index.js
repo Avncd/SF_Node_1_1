40dc9a1497cf34a289114331e72088af39dd40c3
Avncd <hunter@hthompson.me>
2017-05-20 16:39:13 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,6 @@ exports = module.exports = function (app) {
 	app.get('/gallery', routes.views.gallery);
 	app.get('/products/:product', routes.views.product);
 	app.get('/products', routes.views.products);
-	app.get('/contactform', routes.views.contactform);
 	app.all('/contact', routes.views.contact);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
